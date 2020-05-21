8a813ab74a793a0ea6855810609081836b8f290e
Markiian Matsiuk <matsiuk@ucu.edu.ua>
2020-05-21 17:18:22 +0300

    added result saving to s3

---
+++ config.py
@@ -0,0 +1,12 @@
+AWS_CREDENTIALS = {
+    "aws_access_key_id": "",
+    "aws_secret_access_key": "",
+    "aws_session_token": "",
+}
+
+SPARK_MASTER_URI = {
+
+}
+
+S3_DATA_LOCATION = "s3a://mmarkiian.s3/copycat_inc/data/"
+S3_OUTPUT_BUCKET = "mmarkiian.s3"
--- spark_processing.py
+++ spark_processing.py
@@ -1,6 +1,9 @@
 import findspark
 import time
 
+from S3Adapter import S3Adapter
+import config
+
 findspark.init()
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
@@ -13,6 +16,7 @@ from task_5 import task_5
 from task_6 import task_6
 
 spark = SparkSession.builder.getOrCreate()
+s3Adapter = S3Adapter(config.AWS_CREDENTIALS, config.S3_OUTPUT_BUCKET)
 
 
 def process_file(zone, csv_file_name, categories_map):
@@ -25,31 +29,37 @@ def process_file(zone, csv_file_name, categories_map):
 
     print("    TASK 1 Started")
     task_start_time = time.time()
-    task_1(df)
+    result = task_1(df)
     print(f"      Task 1 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/1/{zone}/result.json", result)
 
     print("    TASK 2 Started")
     task_start_time = time.time()
-    task_2(df_proper_date, categories_map)
+    result = task_2(df_proper_date, categories_map)
     print(f"      Task 2 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/2/{zone}/result.json", result)
 
     print("    TASK 3 Started")
     task_start_time = time.time()
-    task_3(df_proper_date)
+    result = task_3(df_proper_date)
     print(f"      Task 3 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/3/{zone}/result.json", result)
 
     print("    TASK 4 Started")
     task_start_time = time.time()
-    task_4(df)
+    result = task_4(df)
     print(f"      Task 4 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/4/{zone}/result.json", result)
 
     print("    TASK 5 Started")
     task_start_time = time.time()
-    task_5(df)
+    result = task_5(df)
     print(f"      Task 5 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/5/{zone}/result.json", result)
 
     print("    TASK 6 Started")
     task_start_time = time.time()
-    task_6(df, categories_map)
+    result = task_6(df, categories_map)
     print(f"      Task 6 finished in: {time.time() - task_start_time:.2f}s")
+    s3Adapter.upload_file(f"copycat_inc/6/{zone}/result.json", result)
     print(f"    {zone} zone overall time: {time.time() - start_time:.2f}s")
